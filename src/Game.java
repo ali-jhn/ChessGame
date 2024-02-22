042b60ab87c575426e04c738936883bbf06eec73
Ali Jahan <alijahan890@gmail.com>
2024-02-22 23:36:23 +0330

    feat: newGame and LoadGame buttons added

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
     private JFrame startFrame;
@@ -31,6 +32,74 @@ public class Game {
         startPanel.add(new JLabel());
         startFrame.add(startPanel);
 
+        setButtons(startPanel);
+    }
+
+    private void setButtons(JPanel startPanel){
+        JButton newGame = new JButton("New Game");
+        JButton loadGame = new JButton("Load Game");
+        newGame.setFont(font);
+        loadGame.setFont(font);
+        newGame.setBackground(new Color(85, 136, 188));
+        loadGame.setBackground(new Color(85,136,188));
+        startPanel.add(newGame);
+        startPanel.add(loadGame);
+
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new NewGame();
+                startFrame.setVisible(false);
+            }
+        });
+
+        loadGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new LoadGame();
+                startFrame.setVisible(false);
+            }
+        });
+    }
+
+    public static void startBoard(int[][] arr){ // public becuase i used it in Board class
+        for(int i =0 ; i< 8 ; i+=7 ){
+            for(int j = 0 ; j< 8 ; j++){
+                if(i==0){
+                    if(j == 0 || j == 7)
+                        arr[i][j] = -3;
+                    else if(j == 1 || j== 6)
+                        arr[i][j] = -5;
+                    else if (j == 2 || j == 5)
+                        arr[i][j] = -4;
+                    else if(j == 3)
+                        arr[i][j] = -1;
+                    else
+                        arr[i][j] = -2 ;
+                } else {
+                    if(j == 0 || j == 7)
+                        arr[i][j] = 3;
+                    else if(j == 1 || j== 6)
+                        arr[i][j] = 5;
+                    else if (j == 2 || j == 5)
+                        arr[i][j] = 4;
+                    else if(j == 3)
+                        arr[i][j] = 1;
+                    else
+                        arr[i][j] = 2 ;
+
+                }
+            }
+        }
+        for(int i = 1 ; i < 8 ; i+=5){
+            if(i == 1){
+                for(int j = 0 ; j < 8 ; j++)
+                    arr[i][j] = -6;
+            } else{
+                for(int j = 0 ; j < 8 ; j++)
+                    arr[i][j] = 6;
+            }
+        }
     }
 
 }
