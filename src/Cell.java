b49c645ee5979d1eec5e55be97518361b28dbc22
Ali Jahan <alijahan890@gmail.com>
2024-02-23 22:45:00 +0330

    new methods added

--- src/Cell.java
+++ src/Cell.java
@@ -4,7 +4,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 
-
 public class Cell extends JButton implements MouseListener {
     Board board;
     Piece piece = null;
@@ -26,7 +25,7 @@ public class Cell extends JButton implements MouseListener {
         this.i = x;
         this.j = y;
 
-
+        defaultColor();
         this.setLayout(new BorderLayout());
 
     }
@@ -36,6 +35,7 @@ public class Cell extends JButton implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
     }
+
 
 
     @Override
@@ -54,5 +54,21 @@ public class Cell extends JButton implements MouseListener {
     public void mouseExited(MouseEvent e) {
 
     }
+    //change color to default color
+    public void defaultColor(){
+        if ((i + j) % 2 == 1) {
+            setBackground(new Color(192,192,192));
+        } else {
+            setBackground(new Color(9, 68, 182));
+        }
+    }
+    //remove piece from board
+    public void removePiece() {
+        if (piece != null) {
+            piece.image = null;
+            piece.setVisible(false);
+            piece = null;
 
+        }
+    }
 }
