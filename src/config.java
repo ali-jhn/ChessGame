763a135fdfec740841f382b2506734ac52953020
Ali Jahan <alijahan890@gmail.com>
2024-02-23 23:01:15 +0330

    Loading the previous game by this class

---
+++ src/config.java
@@ -0,0 +1,7 @@
+class config{
+    int[][] arr = new int[8][8];
+    int turn ; // if it was 0 --> white turn   1--> black turn
+    int ai_Player_Is_Playing; // if it was 0 --> ai is not playing  ; if 1 --> ai is playing
+    int ai_Player_Is_White; // if it was 0 --> ai player is not white ; if 1 --> ai is white
+
+}
