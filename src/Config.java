1037fb9258631a9f2edde9e8fdc7b00da5c9b79b
Ali Jahan <alijahan890@gmail.com>
2024-02-26 12:35:16 +0330

    refactor: change in class name

--- src/Config.java
+++ src/Config.java
@@ -1,4 +1,4 @@
-class config{
+class Config{
     int[][] arr = new int[8][8];
     int turn ; // turn equals 0 if white turn, and equals 1 if black turn
     int ai_Player_Is_Playing; // 1 --> playing with ai mode, 0 --> playing with user
